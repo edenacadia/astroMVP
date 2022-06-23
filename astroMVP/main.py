12b8007d598434207af123db2a8dd858f34c23c0
Eden A <eden.a.mcewen@gmail.com>
2022-06-23 11:43:25 -0700

    Added publication files

--- astroMVP/main.py
+++ astroMVP/main.py
@@ -7,8 +7,6 @@ keyword_input,token_input = display.input()
 print(token_input)
 import PaperSearch as ps
 
-
-
 search1 = ps.PaperSearch(keyword_input[0], token_input[0])
 if search1.paper:
     print("bibcode:", search1.paper)
@@ -18,14 +16,11 @@ if search1.paper:
     authors= paper_info[1]
     abstract = paper_info[2]  
 
-
-
 else: 
     print("No papers found")
     title = 'no title found for ' + keyword_input
     authors= 'NA'
     abstract = 'NA'
-
 
 
 display.output(title,authors,abstract) 
