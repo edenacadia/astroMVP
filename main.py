a673a37c01c5b12bc848692d25a5d7d7a321c2a2
Candice Stauffer <56940363+cosmiccandice@users.noreply.github.com>
2022-06-23 13:00:12 -0500

    commit

--- main.py
+++ main.py
@@ -2,5 +2,16 @@
 # Here we will write the run? files
 
 import display 
-from display import token_input, keyword_input 
-token_input,keyword_input = token_input[0],keyword_input[0] #now, can run these values in papersearch
+
+keyword_input,token_input = display.input()
+
+""" 
+1. input keyword_put and token_input into Papersearch
+2. papersearch outputs title, author, and abstract 
+3. put paper, author, and abtract into output display function
+""" 
+"""FOR EXAMPLE: """
+# title = paper_info[0]
+# authors= paper_info[1]
+# abstract = paper_info[2] 
+#display.output(title,authors,abstract) 
